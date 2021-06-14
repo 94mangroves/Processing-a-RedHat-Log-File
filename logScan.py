b2ebd6bed2429a9e530e643a259092fb99a91488
94 Mangroves <85852190+94mangroves@users.noreply.github.com>
2021-06-13 20:23:36 -0400

    Update logScan.py

--- logScan.py
+++ logScan.py
@@ -1,8 +1,26 @@
+# The purpose of this script is to search for words or strings in a txt document
+# The text document should be in the same folder that this script resides
+#
+# This scipt will prompt the user for a file to process.
+# It will open the file and iterate through each line of the file, catch and report any errors that occur.
+# For each line in the file:
+# If the line identifies a specific worm that was detected, then record the unique name of the worm.
+# Keep track of the number of occurrences of each unique worm.
+# Once all the lines have been processed produce a prettytable result that includes the name of each unique worm and number of occurrences that were identified.
+# The prettytable will format the results by highest occurring worms
+#
+#
+# Note: the best way I found for this to work is by:
+# First, downloading Python in Ubuntu https://phoenixnap.com/kb/how-to-install-python-3-ubuntu
+# Second, downloading and installing Wing IDE in Ubuntu https://wingware.com/doc/install/linux-installation-detail
+# Third, install PIP on Ubuntu https://linuxize.com/post/how-to-install-pip-on-ubuntu-20.04/
+# Fourth, downloading and installing PrettyTables in Ubuntu https://pypi.org/project/prettytable/
 
 # This imports RegEx just in case we don't know the format of the document the user chooses
 
 import re
 
+# This imports PrettyTable, it should be installed locally
 ''' 
 Import Optional PrettyTable Library
 to install from the windows command line or linux/mac terminal
